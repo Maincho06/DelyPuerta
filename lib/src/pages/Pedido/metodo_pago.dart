d14b3295f17d23aa497a72a118e10fc4a3ef6282
DIEGO <dice9030@gmail.com>
2020-08-09 15:28:59 -0500

    .

--- lib/src/pages/Pedido/metodo_pago.dart
+++ lib/src/pages/Pedido/metodo_pago.dart
@@ -2,7 +2,9 @@ import 'package:delipuerta/src/pages/Pedido/pago_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 final int size = 40;
+
 class MetodoPagoPage extends StatelessWidget {
   const MetodoPagoPage(this.precioTotal);
   final precioTotal;
@@ -20,8 +22,8 @@ class MetodoPagoPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text
-              ('Tarjeta',
+              Text(
+                'Tarjeta',
                 style: GoogleFonts.portLligatSans(
                   textStyle: Theme.of(context).textTheme.display1,
                   fontSize: 20,
@@ -29,9 +31,11 @@ class MetodoPagoPage extends StatelessWidget {
                   color: Color(0xffe46b10),
                 ),
               ),
-              Expanded(child:  _SvgPage('assets/pedidos/tarjeta.svg',"TARJETA",precioTotal)),
-              Text
-              ('Efectivo',
+              Expanded(
+                  child: _SvgPage(
+                      'assets/pedidos/tarjeta.svg', "TARJETA", precioTotal)),
+              Text(
+                'Efectivo',
                 style: GoogleFonts.portLligatSans(
                   textStyle: Theme.of(context).textTheme.display1,
                   fontSize: 20,
@@ -39,9 +43,11 @@ class MetodoPagoPage extends StatelessWidget {
                   color: Color(0xffe46b10),
                 ),
               ),
-              Expanded(child:  _SvgPage('assets/pedidos/efectivo.svg',"EFECTIVO",precioTotal)),
-              Text
-              ('Yape',
+              Expanded(
+                  child: _SvgPage(
+                      'assets/pedidos/efectivo.svg', "EFECTIVO", precioTotal)),
+              Text(
+                'Yape',
                 style: GoogleFonts.portLligatSans(
                   textStyle: Theme.of(context).textTheme.display1,
                   fontSize: 20,
@@ -49,64 +55,63 @@ class MetodoPagoPage extends StatelessWidget {
                   color: Color(0xffe46b10),
                 ),
               ),
-              Expanded(child:  _PngPage('assets/pedidos/yape.png',"YAPE",precioTotal)),
+              Expanded(
+                  child:
+                      _PngPage('assets/pedidos/yape.png', "YAPE", precioTotal)),
             ],
           ),
         ),
       ),
     );
   }
-
-  
-
-  
 }
 
 class _SvgPage extends StatelessWidget {
   final String svg;
   final String tipo;
   final double precioTotal;
-  const _SvgPage(this.svg, this.tipo,this.precioTotal);
+  const _SvgPage(this.svg, this.tipo, this.precioTotal);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: EdgeInsets.only(left: 50.0,bottom: 50.0,right: 50.0,top: 20.0),
-        child: SvgPicture.asset(svg)
-      ),
+          width: double.infinity,
+          height: double.infinity,
+          padding:
+              EdgeInsets.only(left: 50.0, bottom: 50.0, right: 50.0, top: 20.0),
+          child: SvgPicture.asset(svg)),
       onTap: () {
         Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => PagoPage(tipo,precioTotal))
-        );
+            context,
+            MaterialPageRoute(
+                builder: (context) => PagoPage(tipo, precioTotal)));
       },
       splashColor: Color(0xffe46b10),
     );
   }
 }
+
 class _PngPage extends StatelessWidget {
   final String png;
   final String tipo;
   final double precioTotal;
-  const _PngPage(this.png,this.tipo,this.precioTotal);
+  const _PngPage(this.png, this.tipo, this.precioTotal);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: EdgeInsets.only(left: 50.0,bottom: 50.0,right: 50.0,top: 20.0),
-        child: Image.asset(png)
-      ),
+          width: double.infinity,
+          height: double.infinity,
+          padding:
+              EdgeInsets.only(left: 50.0, bottom: 50.0, right: 50.0, top: 20.0),
+          child: Image.asset(png)),
       onTap: () {
         Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => PagoPage(tipo,precioTotal))
-        );
+            context,
+            MaterialPageRoute(
+                builder: (context) => PagoPage(tipo, precioTotal)));
       },
       splashColor: Color(0xffe46b10),
     );
@@ -116,9 +121,11 @@ class _PngPage extends StatelessWidget {
 Size screenSize(BuildContext context) {
   return MediaQuery.of(context).size;
 }
+
 double screenHeight(BuildContext context, {double dividedBy = 1}) {
   return screenSize(context).height / dividedBy;
 }
+
 double screenWidth(BuildContext context, {double dividedBy = 1}) {
   return screenSize(context).width / dividedBy;
 }
